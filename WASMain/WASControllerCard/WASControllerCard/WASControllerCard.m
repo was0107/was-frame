451a48d651129ded0e42375af05f9404022f329e
goodluck.was@gmail.com <goodluck.was@gmail.com@56a2e18a-11d8-c952-03c7-3a21f205262e>
2014-08-08 11:20:10 +0000

    commit for backup

--- WASMain/WASControllerCard/WASControllerCard/WASControllerCard.m
+++ WASMain/WASControllerCard/WASControllerCard/WASControllerCard.m
@@ -360,7 +360,7 @@
         case UIGestureRecognizerStateChanged:
         {
             float yPostion = location.y - self.panOriginOffsetY;
-            [self setYCoordinate:yPostion <= 0 ? : yPostion];
+            [self setYCoordinate:yPostion <= 0 ? 0 : yPostion];
             if ((WASControllerCardStateFullScreen == self.state && self.frame.origin.y < _originY) ||
                 (WASControllerCardStateDefault == self.state )) {
                 if ([self.delegate respondsToSelector:@selector(controllerCard:didUpdatePanPercentage:)]) {
